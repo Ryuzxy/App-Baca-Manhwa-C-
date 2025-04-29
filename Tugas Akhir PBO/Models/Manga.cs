8120f471fe940341ffa378f5e32acc9c94f21a31
Ryuxy <Ryuxy@SIAPAHAY0>
2025-04-29 09:36:42 +0700

    Add project files.

---
+++ Tugas Akhir PBO/Models/Manga.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Akhir_PBO.Models
+{
+    public class Manga
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public string CoverImage { get; set; }
+        public int GenreId { get; set; }
+    }
+}
